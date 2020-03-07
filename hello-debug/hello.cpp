35c79730dc94296148bc076d29106a0101c02fd6
Tamas Fulop <tamas.flp@gmail.com>
2020-03-07 18:35:10 +0100

    Update VS config, add some variables to the code

--- hello-debug/hello.cpp
+++ hello-debug/hello.cpp
@@ -3,6 +3,17 @@ using namespace std;
 
 int main() 
 {
-    cout << "Hello, World!";
+    // Some variables to check values
+    int a,b;
+    float e,f;
+    std::string str;
+
+    a = 1;
+    b = 2;
+    e = 3.14;
+    f = 1234.5678;
+    str = ":)";
+
+    cout << "Hello, I am being debugged!";
     return 0;
 }
